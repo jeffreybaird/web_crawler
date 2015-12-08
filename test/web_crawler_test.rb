f630682ef74998c4861da9509a6535c710bab1c2
jeffreybaird <jbaird@sweetspotdiabetes.com>
2015-12-08 10:36:01 -0800

    initial commit

---
+++ test/web_crawler_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class WebCrawlerTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::WebCrawler::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
